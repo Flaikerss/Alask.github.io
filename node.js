7215891f5b42a230187a1ec197eb7b92ac3650b9
Flaikerss <banduil05@mail.ru>
2025-02-12 13:28:30 +0300

    save

--- node.js
+++ node.js
@@ -1,1 +1,2 @@
-Token = '7253299607:AAG9VjK5sWjVFxrjCH0CJFCUkpg1wSv9pzw'
+const Token = '7253299607:AAG9VjK5sWjVFxrjCH0CJFCUkpg1wSv9pzw'
+const webAppUrl = 'https://web.telegram.org/k/#@AlaslaMesh_bot'
